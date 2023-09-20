36941f28f33f5bffcfb0f35d7c72c9ab32536cd9
Sadra Bigdeli <sadra.bigdeli1350@gmail.com>
2023-09-20 17:31:13 +0330

    handle event click cancel and newfolder

--- src/components/Navbar/NavBarItem.jsx
+++ src/components/Navbar/NavBarItem.jsx
@@ -1,5 +1,5 @@
 function NavBarItem({ text, icon, selected, onClick }) {
-  const backgroundColorSelected = {backgroundColor: selected ? "#FFFBFA" : "#F8EEE2"};
+  const backgroundColorSelected = {backgroundColor: selected && "#FFFBFA"};
   return (
     <div className="nav-bar-item" style={backgroundColorSelected} onClick={onClick}>
       <img src={icon} />
--- src/components/Navbar/NavbarFolders.jsx
+++ src/components/Navbar/NavbarFolders.jsx
@@ -6,39 +6,64 @@ import { useState } from "react";
 
 const folders = [
   {
-    id : 1,
+    id: 1,
     name: "همه یاد‌داشت‌‌ها",
   },
   {
-    id : 2,
+    id: 2,
     name: "کاری",
   },
   {
-    id : 3,
+    id: 3,
     name: "مسافرت",
   },
   {
-    id : 4,
+    id: 4,
     name: "رویدادها",
   },
   {
-    id : 5,
+    id: 5,
     name: "باشگاه",
   },
 ];
 
 function Folders() {
   const [Selected, setSelected] = useState(null);
+  const [isShowNewFolder, setIsShowNewFolder] = useState(false);
+  
+  const handleClick = () => {
+    setIsShowNewFolder(true);
+ 
+  };
+  const handleCancelNewFolder = () => {
+    setIsShowNewFolder(false);
+ 
+  };
+  const handleCreateFolder = () => {
+    console.log("ssd");
+  };
+  const handleChangeInput = (e) => {
+    console.log("ssd" , e.target.value);
+  };
   return (
     <section className="folders-container">
       <header>
         <h2>پوشه‌ها</h2>
-        <button className="new-folder-button">
+        <button className="new-folder-button" onClick={handleClick}>
           <img src={NewFolderIcon} />
         </button>
       </header>
 
       <div className="folders">
+        {isShowNewFolder && (
+          <div className="nav-bar-new-folder">
+            <img src={folderIcon} />
+            <input type="text" placeholder="Name..." onChange={handleChangeInput}/>
+            <button onClick={handleCreateFolder}>Ok</button>
+            <button onClick={handleCancelNewFolder}>cancel</button>
+          </div>
+        )}
+
         {folders.map((item) => (
           <NavBarItem
             key={item.id}
